3e077107a1b3d41d7aa1aa4e4fae9abd2b61cbee
charles.local <charles.local@lo-mh-15-029.lowell.local>
2018-10-25 15:28:45 -0700

    Test first commit - created file and staged changed.

---
+++ sort.py
@@ -0,0 +1,10 @@
+#This is a script to sort our camera jpg's into folders by day.
+#We could probably use bash but I'm trying to learn git (and python) so here we are.
+import datetime
+import os
+#loop through each file.  create directory based on filename with defined structure
+#for the days.
+path = 'C:/tmp/godo_timelapse' #set path for sorting pictures
+
+
+
