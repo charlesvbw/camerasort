d2cfedfbe95cb66a7f5eb3b3b51e50e0f9d85cdb
charles.local <charles.local@lo-mh-15-029.lowell.local>
2018-10-25 15:50:06 -0700

    More git testing

--- sort.py
+++ sort.py
@@ -5,6 +5,11 @@ import os
 #loop through each file.  create directory based on filename with defined structure
 #for the days.
 path = 'C:/tmp/godo_timelapse' #set path for sorting pictures
-
+for file in os.listdir(path):
+    file_day = file[15:17]
+    file_month = file[13:15]
+    folder = file_month + "-" + file_day
+    if os.path.isdir(path + folder):
+        
 
 
